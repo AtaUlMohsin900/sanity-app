9617d7e083d049ec1cb611edd4f49fa912e31b7c
AtaUlMohsin <ataulmohsin900@gmail.com>
2023-09-01 20:14:34 +0500

    first commit

---
+++ sanity/products.ts
@@ -0,0 +1,14 @@
+
+// sanity/pet.ts
+export default {
+    name: 'products',
+    type: 'document',
+    title: 'Products',
+    fields: [
+        {
+            name: 'title',
+            type: 'string',
+            title: 'Name'
+        }
+    ]
+}
---
+++ sanity/schema.ts
@@ -0,0 +1,5 @@
+import { type SchemaTypeDefinition } from 'sanity'
+import products from "./products"
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [products],
+}
